5788bcb1d1694f35db65ec7d2012d60263ba35bf
Neeraj-kurmi <neerajkurmi2002@gmail.com>
2024-11-25 15:25:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import { useState } from 'react'
+import { items } from './items'
+
+function App() {
+  const [leftItems, setLeftItems] = useState(items)
+  const [rightItems,setRightItems]=useState([])
+  
+  const helper=(list,id,checked)=>{
+    return list.map((item)=>{
+      if(item.id===id){
+        return {
+          ...item,
+          checked:!checked
+        }
+      }
+      return item
+    })
+  }
+  const handleClick=(id,checked,direction)=>{
+      if(direction==='left'){
+        let copylist=[...leftItems];
+        copylist=helper(copylist,id,checked);
+        setLeftItems(copylist)
+      }else{
+        let copylist=[...rightItems];
+        copylist=helper(copylist,id,checked);
+        setRightItems(copylist)
+      }
+  }
+  const  onSubmitHandler=(direction)=>{
+      if(direction==='right'){
+        let check=leftItems.filter((item)=>item.checked)
+        let uncheck=leftItems.filter((item)=>!item.checked)
+        let res=check.map((item)=>{
+          return {
+            ...item,
+            checked:!item.checked
+          }
+        })
+        let bucket=[...rightItems,...res]
+        setRightItems(bucket);
+        setLeftItems(uncheck)
+        
+      }else{
+        let check=rightItems.filter((item)=>item.checked)
+        let uncheck=rightItems.filter((item)=>!item.checked)
+        let res=check.map((item)=>{
+          return {
+            ...item,
+            checked:!item.checked
+          }
+        })
+        let bucket=[...leftItems,...res]
+        setLeftItems(bucket);
+        setRightItems(uncheck)
+      }
+  }
+  return (
+    <>
+    <h1 className='text-center pt-40 text-lg font-bold text-red-800'>Select Item From one bucket and Shift to Another </h1>
+    <div className='flex text-center justify-center mt-40 items-center'>
+      <div className='flex flex-col gap-2 bg-zinc-500 w-44 h-40 p-1'>
+         {
+          leftItems.map((item,index)=>(
+            <div 
+            className={`${item.checked?"bg-black":"bg-amber-600"} mx-auto p-1 rounded-lg text-white cursor-pointer`}
+            key={index}
+            onClick={()=>handleClick(item.id,item.checked,'left')}
+            >{item.name}</div>
+          ))
+         }
+      </div>
+      <div className='flex flex-col'>
+      <button className=' bg-stone-700 text-white p-1 rounded-lg m-2 hover:scale-90'
+      onClick={()=>onSubmitHandler('right')}
+      >Right</button>
+      <button className=' bg-stone-700 text-white p-1 rounded-lg m-2 hover:scale-90'
+       onClick={()=>onSubmitHandler('left')}
+      >Left</button>
+      </div>
+      <div className='flex flex-col gap-2 bg-zinc-500 w-44 h-40 p-1'>
+         {
+          rightItems.map((item,index)=>(
+            <div 
+            className={`${item.checked?"bg-black":"bg-amber-600"} mx-auto p-1 rounded-lg text-white cursor-pointer`}
+            key={index}
+            onClick={()=>handleClick(item.id,item.checked,'right')}
+            >{item.name}</div>
+          ))
+         }
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/items.jsx
@@ -0,0 +1,6 @@
+export const items=[
+    {name:"First",id:0,checked:false},
+    {name:"Second",id:1,checked:false},
+    {name:"Third",id:2,checked:false},
+    {name:"Fourth",id:3,checked:false}
+]
